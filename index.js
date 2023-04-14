5b9426a8eef723b477b64c428a395ecab0c63495
JarvisAI <65456447+GruntingRhino@users.noreply.github.com>
2023-04-14 18:50:49 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
-var datetime = new Date();
-console.log(datetime);
-document.getElementById("time").textContent = datetime;
+function refreshTime() {
+  var datetime = new Date();
+  console.log(datetime);
+  document.getElementById("time").textContent = datetime;
+}
+setInterval(refreshTime, 1000);
