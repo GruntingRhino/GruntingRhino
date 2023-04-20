f26b68f6db0e15114e5bf8fedaa43816f6ca3adf
JarvisAI <65456447+GruntingRhino@users.noreply.github.com>
2023-04-19 20:50:10 -0400

    Update index.js

--- index.js
+++ index.js
@@ -5,4 +5,9 @@ function refreshTime() {
   const formattedString = dateString.replace(", ", " - ");
   timeDisplay.textContent = formattedString;
 }
-setInterval(refreshTime, 1000); 
+setInterval(refreshTime, 1000);
+
+var api_key = 'b9d02b5a053194b54ec34746fd86542f';
+results.loadhtml('http://api.openweathermap.org/data/2.5/weather?q=Montreal&mode=html&appid=' + api_key);
+results.append("Weather For: " + weather_data.name + '<br/>');
+results.append("Current: " + weather_data.main.temp + '&deg;F<br/>');
